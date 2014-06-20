84f1519d6e22189af09089fb173276446fd36adf
Stephen Kennedy <stephen@devenable.com>
2014-06-21 00:01:41 +1000

    Initial commit based off template with some renames and user login

---
+++ CounterWtf.Service/Global.asax.cs
@@ -0,0 +1,10 @@
+﻿namespace CounterWtf.Service
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            WebApiConfig.Register();
+        }
+    }
+}
